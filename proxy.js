6620d27ad40564993a83db05ab94bfdaee03d30a
Greg Scott <gmscott27@gmail.com>
2013-07-28 19:01:02 -0400

    Fixed Proxy errors

--- proxy.js
+++ proxy.js
@@ -7,17 +7,17 @@ httpProxy.createServer({
   router: {
     
     //Aethyrnet
-    'aethyrnet.com'           : 'www.aethyrnet.com',
+    'aethyrnet.com'           : '127.0.0.1:8000',
     'www.aethyrnet.com'       : '127.0.0.1:8000',
     'test.aethyrnet.com'      : '127.0.0.1:8080',
     
     //Scribl
-    'scriblnews.com'          : 'www.scriblnews.com',
+    'scriblnews.com'          : '127.0.0.1:8001',
     'www.scriblnews.com'      : '127.0.0.1:8001',
     'test.scriblnews.com'     : '127.0.0.1:8081',
     
     //Mafia
-    'mafiaplus.net'           : 'www.mafiaplus.net',
+    'mafiaplus.net'           : '127.0.0.1:8002',
     'www.mafiaplus.net'       : '127.0.0.1:8002',
     'test.mafiaplus.net'      : '127.0.0.1:8082',
     
